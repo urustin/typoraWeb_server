81ae233e164c6b86a21f0714db73e5d13047e7cd
Eric Son <urustin@naver.com>
2024-07-26 21:42:06 +0900

    callbackURL

--- config/passport.js
+++ config/passport.js
@@ -44,7 +44,7 @@ passport.use(new LocalStrategy({
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 async function(accessToken, refreshToken, profile, done) {
   console.log("Google Strategy Callback");
